347c242fb49a5a8ac3a9c44b3cbabf7bfc30809b
Pavel Morozov <morozov_p.a@bk.ru>
2021-03-05 15:11:19 +0300

    Create model Book

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int ID { get; }
+        public string Title { get; }
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+    }
+}
